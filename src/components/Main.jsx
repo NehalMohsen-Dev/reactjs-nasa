d35c31af7fbf77425dfca9dfac4762e1e7da9133
Nehal Mohsen <58566870+nehalmohsen22@users.noreply.github.com>
2025-04-06 21:47:59 +0200

    Adding main - footer - sidebar Components & styles

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='imageContainer'>
+        <img src='mars.png' alt='mars-demo-pic'className='bgImage' />
+      
+    </div>
+  )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='sidebar'>
+      <div className="bgOverlay"></div>
+      <div className="sidebarContent">
+        <h2>
+            The Brutal Martian Landscape
+        </h2>
+        <div className="">
+
+        <p> Description</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolor repudiandae aliquam. Nisi exercitationem, minima eaque dolorum doloremque earum sed repellendus a ducimus unde magnam in cum inventore omnis esse!</p>
+        
+        </div>
+        <button> 
+        <i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+  )
+}
